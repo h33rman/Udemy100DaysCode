f56fd076e55b44668dbde9248cb8f55e5372db8c
her7man <herman.andriam@gmail.com>
2023-09-22 10:04:26 +0300

    D07

--- Day07/Day07-Project.py
+++ Day07/Day07-Project.py
@@ -1,19 +1,19 @@
 # HANG AN game
-import hangman_art
-import hangman_words
-
-
-word_list = hangman_words.word_list
-stages = hangman_art.stages
-logo = hangman_art.logo
-
-
+from hangman_art import stages, logo
+from hangman_words import word_list
 import random
+import os
+
+def clear():
+    os.system('clear')
+
 chosen_word = random.choice(word_list)
 
 display = []
+
 for letter in range(len(chosen_word)):
     display.append("_")
+
 print(display)
 
 UserLives = 6
@@ -24,16 +24,25 @@ print(logo)
 while GameStatus==False:
     
     guess = input("Guess a letter: ").lower()
+    clear()
+    
+    if guess in display:
+        print(f"You have already guessed {guess}")
+
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+
         UserLives -= 1
         print(stages[UserLives])
         if UserLives == 0:
             GameStatus = True
             print("You lose")
+
     if "_" not in display:
         GameStatus = True
         print("You win")

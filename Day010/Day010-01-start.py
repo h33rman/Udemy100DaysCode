e10409feb3c3f31238faeae355f2d7997da75419
her7man <herman.andriam@gmail.com>
2024-03-18 10:42:18 +0300

    Day 10

---
+++ Day010/Day010-01-start.py
@@ -0,0 +1,12 @@
+# Use of functions with outputs
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+f_name = input("What is your first name? ")
+l_name = input("What is your last name? ")
+print(format_name(f_name, l_name))

6563ab0374007d474da3b0c1da3f8472a5f8246a
her7man <herman.andriam@gmail.com>
2024-03-22 14:58:53 +0300

    day 11

--- Day010/Day010-03-Calculator.py
+++ Day010/Day010-03-Calculator.py
@@ -1,6 +1,4 @@
 # Calculator
-import art
-
 
 # Add
 def add(n1, n2):
@@ -27,9 +25,8 @@ operations = {
 }
 
 def calculator():
-    print(art.logo)
 
-    num1 = int(input("What's the first number?: "))
+    num1 = float(input("What's the first number?: "))
     for symbol in operations:
         print(symbol)
 
@@ -37,7 +34,7 @@ def calculator():
 
     while should_continue:
         operation_symbol = input("Pick an operation from the line above: ")
-        num2 = int(input("What's the second number?: "))
+        num2 = float(input("What's the second number?: "))
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")

1382ad43efffb84be54614e41c7349121bc9cc1a
her7man <herman.andriam@gmail.com>
2024-04-04 17:40:45 +0300

    End DA12

--- Day012/Day012-01-start.py
+++ Day012/Day012-01-start.py
@@ -7,3 +7,14 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+
+# Global Scope
+player_health = 10
+# print(potion_strength) # NameError: name 'potion_strength' is not defined

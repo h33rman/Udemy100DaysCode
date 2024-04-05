044a81c99f59ca596ce6826532623ea7c4cf86a7
her7man <herman.andriam@gmail.com>
2024-04-05 11:31:26 +0300

    Day13 tips debug

---
+++ Day013/Day013-01-Debug.py
@@ -0,0 +1,63 @@
+###########DEBUGGING#####################
+
+# Describe Problem
+print("Describe Problem")
+def my_function():
+  for i in range(1, 21):
+ 
+    if i == 20:
+      print("You got it")
+my_function()
+print("-----------------------------")
+
+# Reproduce the Bug
+print("Reproduce the Bug")
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0,5)
+print(dice_imgs[dice_num])
+print("-----------------------------")
+
+# Play Computer
+print("Play Computer")
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
+print("-----------------------------")
+
+# Fix the Errors
+print("Fix the Errors")
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+
+print("-----------------------------")
+
+#Print is Your Friend
+print("Print is Your Friend")
+pages = 0
+word_per_page = 0
+
+pages = int(input("Number of pages: "))
+print(f"pages: {pages}")
+
+word_per_page = int(input("Number of words per page: "))
+print(f"word_per_page: {word_per_page}")
+
+total_words = pages * word_per_page
+print(total_words)
+print("-----------------------------")
+
+#Use a Debugger
+print("Use a Debugger")
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
+print("-----------------------------")

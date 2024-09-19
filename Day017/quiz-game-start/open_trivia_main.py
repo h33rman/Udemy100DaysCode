15ae92cb59a59c493e1c58c47ebe6877e551dbb4
h33rman <herman.andriam@gmail.com>
2024-09-19 17:24:13 +0300

    day 17 end

--- Day017/quiz-game-start/open_trivia_main.py
+++ Day017/quiz-game-start/open_trivia_main.py
@@ -1,13 +1,13 @@
 from question_model import Question
-from data import question_data
+from open_trivia_db import trivia_db
 from quiz_brain import QuizBrain
 
 
 question_bank = []
 
-for question in question_data:
+for question in trivia_db:
     question_text = question["question"]
-    question_answer = question["answer"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
